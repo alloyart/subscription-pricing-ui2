782dd816dc5c4f27bca9bf0a190ae01f66794534
alloyart <1354829128@qq.com>
2026-09-15 00:36:57 +0800

    Remove development server logging

--- server.mjs
+++ server.mjs
@@ -26,6 +26,4 @@ createServer(async (req, res) => {
     res.writeHead(404, { 'Content-Type': 'text/plain; charset=utf-8' });
     res.end('Not found');
   }
-}).listen(port, '127.0.0.1', () => {
-  console.log(`subscription-pricing-ui2 running at http://127.0.0.1:${port}`);
-});
+}).listen(port, '127.0.0.1');
